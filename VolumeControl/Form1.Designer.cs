4092a7a14067c995e4993e99b760c5775834ba7e
Administrator <453843655@qq.com>
2024-11-22 16:14:41 +0800

    整体提交

---
+++ VolumeControl/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace VolumeControl
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            volumeLabel = new Label();
+            label4 = new Label();
+            nameLabel = new Label();
+            muteCheck = new CheckBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(121, 118);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 17);
+            label1.TabIndex = 0;
+            label1.Text = "'Alt' + '+'增加音量";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(121, 135);
+            label2.Name = "label2";
+            label2.Size = new Size(105, 17);
+            label2.TabIndex = 1;
+            label2.Text = "'Alt' + '-'减少音量";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(61, 63);
+            label3.Name = "label3";
+            label3.Size = new Size(68, 17);
+            label3.TabIndex = 2;
+            label3.Text = "当前音量：";
+            // 
+            // volumeLabel
+            // 
+            volumeLabel.AutoSize = true;
+            volumeLabel.Font = new Font("Microsoft YaHei UI", 14F);
+            volumeLabel.Location = new Point(123, 59);
+            volumeLabel.Name = "volumeLabel";
+            volumeLabel.Size = new Size(23, 25);
+            volumeLabel.TabIndex = 3;
+            volumeLabel.Text = "0";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(56, 17);
+            label4.TabIndex = 4;
+            label4.Text = "当前设备";
+            // 
+            // nameLabel
+            // 
+            nameLabel.AutoSize = true;
+            nameLabel.Location = new Point(12, 29);
+            nameLabel.Name = "nameLabel";
+            nameLabel.Size = new Size(0, 17);
+            nameLabel.TabIndex = 5;
+            // 
+            // muteCheck
+            // 
+            muteCheck.AutoSize = true;
+            muteCheck.Location = new Point(61, 83);
+            muteCheck.Name = "muteCheck";
+            muteCheck.Size = new Size(63, 21);
+            muteCheck.TabIndex = 6;
+            muteCheck.Text = "已静音";
+            muteCheck.UseVisualStyleBackColor = true;
+            muteCheck.CheckedChanged += muteCheck_CheckedChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(242, 161);
+            Controls.Add(muteCheck);
+            Controls.Add(nameLabel);
+            Controls.Add(label4);
+            Controls.Add(volumeLabel);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            ShowInTaskbar = false;
+            Text = "音量控制";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label volumeLabel;
+        private Label label4;
+        private Label nameLabel;
+        private CheckBox muteCheck;
+    }
+}
---
+++ VolumeControl/Form1.cs
@@ -0,0 +1,157 @@
+using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace VolumeControl
+{
+    public partial class Form1 : Form
+    {
+        // Import the user32.dll for registering and unregistering hotkeys
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        // Hotkey identifiers
+        private const int HOTKEY_ID_INCREASE = 1;
+        private const int HOTKEY_ID_DECREASE = 2;
+
+        // Modifier keys
+        private const uint MOD_ALT = 0x0001;
+
+        // Virtual key codes
+        private const uint VK_OEM_PLUS = 0xBB; // '+'
+        private const uint VK_OEM_MINUS = 0xBD; // '-'
+
+        private MMDevice device;
+        private MMDeviceEnumerator deviceEnumerator;
+        private NotificationClient notificationClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            deviceEnumerator = new MMDeviceEnumerator();
+            notificationClient = new NotificationClient(this);
+            deviceEnumerator.RegisterEndpointNotificationCallback(notificationClient);
+            device = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            device.AudioEndpointVolume.OnVolumeNotification += OnVolumeNotification;
+            GetDeviceInfo();
+            RegisterHotKeys();
+        }
+
+        public void GetDevice()
+        {
+            if (device != null)
+            {
+                device.AudioEndpointVolume.OnVolumeNotification -= OnVolumeNotification;
+            }
+            device = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+            device.AudioEndpointVolume.OnVolumeNotification += OnVolumeNotification;
+        }
+
+        private void RegisterHotKeys()
+        {
+            if (!RegisterHotKey(this.Handle, HOTKEY_ID_INCREASE, MOD_ALT, VK_OEM_PLUS))
+            {
+                MessageBox.Show("'Alt' + '+'热键注册失败", "警告");
+            }
+            if (!RegisterHotKey(this.Handle, HOTKEY_ID_DECREASE, MOD_ALT, VK_OEM_MINUS))
+            {
+                MessageBox.Show("'Alt' + '-'热键注册失败", "警告");
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            // Handle the hotkey events
+            if (m.Msg == 0x0312) // WM_HOTKEY
+            {
+                switch (m.WParam.ToInt32())
+                {
+                    case HOTKEY_ID_INCREASE:
+                        ChangeVolume(0.01f); // Increase volume by 1%
+                        break;
+                    case HOTKEY_ID_DECREASE:
+                        ChangeVolume(-0.01f); // Decrease volume by 1%
+                        break;
+                }
+            }
+            base.WndProc(ref m);
+        }
+
+        private void ChangeVolume(float delta)
+        {
+            float currentVolume = device.AudioEndpointVolume.MasterVolumeLevelScalar;
+            float newVolume = Math.Clamp(currentVolume + delta, 0.0f, 1.0f);
+            device.AudioEndpointVolume.MasterVolumeLevelScalar = newVolume;
+        }
+
+        private void OnVolumeNotification(AudioVolumeNotificationData notificationData)
+        {
+            // 音量变化时更新显示
+            this.Invoke(GetDeviceInfo);
+        }
+
+        public void GetDeviceInfo()
+        {
+            nameLabel.Text = device.FriendlyName;
+            var currentVolume = device.AudioEndpointVolume.MasterVolumeLevelScalar;
+            volumeLabel.Text = Math.Round(currentVolume * 100).ToString();
+            muteCheck.Checked = device.AudioEndpointVolume.Mute;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, HOTKEY_ID_INCREASE);
+            UnregisterHotKey(this.Handle, HOTKEY_ID_DECREASE);
+            device.AudioEndpointVolume.OnVolumeNotification -= OnVolumeNotification;
+            deviceEnumerator.UnregisterEndpointNotificationCallback(notificationClient);
+            base.OnFormClosing(e);
+        }
+
+        private void muteCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            device.AudioEndpointVolume.Mute = muteCheck.Checked;
+        }
+    }
+
+    public class NotificationClient : IMMNotificationClient
+    {
+        private readonly Form1 form;
+
+        public NotificationClient(Form1 form)
+        {
+            this.form = form;
+        }
+
+        public void OnDefaultDeviceChanged(DataFlow dataFlow, Role deviceRole, string defaultDeviceId)
+        {
+            this.form.Invoke(new Action(() =>
+            {
+                this.form.GetDeviceInfo();
+                this.form.GetDevice();
+            }));
+        }
+
+        public void OnDeviceStateChanged(string deviceId, DeviceState newState)
+        {
+        }
+
+        public void OnDeviceAdded(string pwstrDeviceId)
+        {
+        }
+
+        public void OnDeviceRemoved(string deviceId)
+        {
+        }
+
+        public void OnPropertyValueChanged(string pwstrDeviceId, PropertyKey key)
+        {
+        }
+    }
+}
